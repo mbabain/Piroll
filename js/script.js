a3fcf3c6bfed2974da09ba98d936c151126ef4f3
Maksim Babain <mababain@mail.ru>
2020-08-06 19:28:19 +0700

    Fixed resize hamburger bug

--- js/script.js
+++ js/script.js
@@ -22,6 +22,21 @@ const activeSlide = n => {
     items[n].classList.add('active');
 };
 
+const activeOn = function () {
+    navigation.style.cssText = "transition: 0.2s cubic-bezier(0.4, 0, 0.2, 1); visibility: visible; opacity: 1;";
+    hamburgerBackground.style.width = "250px";
+    hamburgerLine1.style.transform = "matrix(0.7071, 0.7071, -0.7071, 0.7071, 0, 4)";
+    hamburgerLine2.style.width = "0px";
+    hamburgerLine3.style.transform = "matrix(0.7071, -0.7071, 0.7071, 0.7071, 0, -4)";
+}
+
+const activeOff = function () {
+    hamburgerBackground.style.width = "0px";
+    hamburgerLine1.style.transform = "matrix(1.00,0.00,0.00,1.00,0,0)";
+    hamburgerLine2.style.width = "100%";
+    hamburgerLine3.style.transform = "matrix(1.00,0.00,0.00,1.00,0,0)";
+}
+
 dots.forEach((item, indexDot) => {
     item.addEventListener('click', () => {
         index = indexDot;
@@ -33,21 +48,34 @@ dots.forEach((item, indexDot) => {
 hamburgerMenu.addEventListener('click', () => {
     hamburgerMenu.classList.toggle('active');
     if (hamburgerMenu.classList.contains('active') == true && window.matchMedia('(max-width: 767px)').matches) {
-        navigation.style.cssText = "visibility: visible; opacity: 1;";
-        hamburgerBackground.style.width = "250px";
-        hamburgerLine1.style.transform = "matrix(0.7071, 0.7071, -0.7071, 0.7071, 0, 4)";
-        hamburgerLine2.style.width = "0px";
-        hamburgerLine3.style.transform = "matrix(0.7071, -0.7071, 0.7071, 0.7071, 0, -4)";
-    } else if (hamburgerMenu.classList.contains('active') == false && window.matchMedia('(max-width: 767px)').matches) {
-        navigation.style.cssText = "visibility: hidden; opacity: 0;";
-        hamburgerBackground.style.width = "0px";
-        hamburgerLine1.style.transform = "matrix(1.00,0.00,0.00,1.00,0,0)";
-        hamburgerLine2.style.width = "100%";
-        hamburgerLine3.style.transform = "matrix(1.00,0.00,0.00,1.00,0,0)";
+        activeOn();
+    } else if (hamburgerMenu.classList.contains('active') == false && window.matchMedia('(max-width: 767px)').matches){
+        activeOff();
+        navigation.style.cssText = "transition: 0.2s cubic-bezier(0.4, 0, 0.2, 1); visibility: hidden; opacity: 0;";
     } else {
         navigation.style.cssText = "visibility: visible; opacity: 1;";
         hamburgerBackground.style.width = "0px";
     }
-    console.log(hamburgerMenu);
-    console.log(hamburgerLine1);
-})
+});
+
+window.addEventListener('scroll', () => {
+    if (pageYOffset > 40) {
+    hamburgerMenu.classList.toggle('active');
+    navigation.style.cssText = "visibility: hidden; opacity: 0;";
+    activeOff();
+    }
+});
+
+window.addEventListener(`resize`, event => {
+    if (window.matchMedia('(min-width: 768px)').matches) {
+        activeOff();
+        hamburgerMenu.classList.remove('active');
+        navigation.style.cssText = "visibility: visible; opacity: 1;";
+        hamburgerBackground.style.width = "0px";
+    } else {
+        hamburgerMenu.classList.remove('active');
+        navigation.style.cssText = "visibility: hidden; opacity: 0;";
+        hamburgerBackground.style.width = "0px";
+        console.log(1);
+    }
+});
